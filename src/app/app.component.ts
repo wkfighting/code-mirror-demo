9977bf3c8c006cb8ed4ee3aa356f42905f476cf2
wukai <is.wukai@outlook.com>
2023-09-02 02:09:44 +0800

    feat: sql 编辑器，支持格式化

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { format } from 'sql-formatter';
+import * as CodeMirror from 'codemirror';
+import 'codemirror/mode/sql/sql';
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/selection/active-line';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'code-mirror-demo';
+export class AppComponent implements OnInit {
+  sql = 'SELECT * FROM customers WHERE age > 18';
+  editor: any;
+
+  ngOnInit(): void {
+    const textarea = document.querySelector('.textarea') as HTMLTextAreaElement;
+    this.editor = CodeMirror.fromTextArea(textarea, {
+      mode: 'text/x-sql',
+      theme: 'idea',
+      lineNumbers: true,
+      styleActiveLine: true,
+      autoCloseBrackets: true,
+    });
+    this.editor.doc.setValue(this.sql);
+  }
+
+  format() {
+    const curSql = this.editor.doc.getValue();
+    const formattedSql = format(curSql, { keywordCase: 'lower' });
+    this.editor.doc.setValue(formattedSql);
+  }
 }
